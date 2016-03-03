7fe729723f59484d32a4981325767a13d748f139
Matt Miller <thisismattmiller@gmail.com>
2016-03-03 14:52:44 -0500

    added standard linter to the tests

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-"use strict"
+'use strict'
 
-function IngestFast() {
-
-	/**
-	 * Will prompt for the location of the FAST data
-	 *
-	 * @param  {function} cb - Nothing returned
-	 */
-	this.ingest = require(__dirname + '/lib/ingest')(this)
-
+function IngestFast () {
+  /**
+   * Will prompt for the location of the FAST data
+   *
+   * @param  {function} cb - Nothing returned
+   */
+  this.ingest = require(`${__dirname}/lib/ingest`)(this)
 }
 
-module.exports = exports = new IngestFast();
+module.exports = exports = new IngestFast()
